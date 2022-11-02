7285a6d8227da1e978772db825b495b653824a9c
Brian Yang <brianyang1206@gmail.com>
2022-11-02 12:04:23 -0400

    File can now be used to open up an emtpy website

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("placeholder.html") #This will be used to show our homescreen
+
+if __name__ == "__main__":
+    app.debug = True #Remove when finished with the project
+    app.run()
