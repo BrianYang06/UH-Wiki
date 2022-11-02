e4c687a4c23cef27811101c8dbcd723c80f7ab53
Brian Yang <brianyang1206@gmail.com>
2022-11-02 12:36:35 -0400

    Added redirects to login/signup page from home

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def home_page():
     return render_template("home_page.html") #This will be used to show our homescreen
 
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup_page():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.debug = True #Remove when finished with the project
     app.run()
