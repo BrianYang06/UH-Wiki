5d65c1db13ee0d5f30605665c6afefbbf3222a5c
Brian Yang <brianyang1206@gmail.com>
2022-11-02 12:08:17 -0400

    Changed template name in __init__.py to accurately reflect the template

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template("placeholder.html") #This will be used to show our homescreen
+    return render_template("home_page.html") #This will be used to show our homescreen
 
 if __name__ == "__main__":
     app.debug = True #Remove when finished with the project
