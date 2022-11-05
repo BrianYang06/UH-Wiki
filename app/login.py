cff3d4ba3b3f5c84017219b79fcebc392198b6fc
Brian Yang <brianyang1206@gmail.com>
2022-11-05 19:36:47 -0400

    testing sqlite db works in adding

--- app/login.py
+++ app/login.py
@@ -1,3 +1,23 @@
 import sqlite3
 
 
+db = sqlite3.connect('login.db')
+c  = db.cursor()
+
+#testing additions
+c.execute("CREATE TABLE IF NOT EXISTS logins(user TEXT, password TEXT, id INTEGER)")
+c.execute("INSERT INTO logins VALUES(?, ?,?)", ("jim", "jimothy", 1)) 
+c.execute("SELECT * FROM logins")
+print(c.fetchall())
+
+#def main(user, passw):
+#	x = login.fetchall()
+#	print(type(x))
+#	print(x)
+
+
+#if __name__ == "__main__":
+#	main('no', 'no')
+
+db.commit()
+db.close()
