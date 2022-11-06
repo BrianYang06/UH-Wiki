cf5214e190e8462c791d3d5b334acbefb30038f6
Brian Yang <brianyang1206@gmail.com>
2022-11-05 21:00:37 -0400

    login.py works to store login info from db

--- app/login.py
+++ app/login.py
@@ -1,13 +1,13 @@
 import sqlite3
-import pprint
+from pprint import pprint
 import sys
 
 db = sqlite3.connect('login.db')
 c  = db.cursor()
 
 #for command line arguments
-#user = sys.argv[0]
-#passw = sys.argv[1]
+
+
 
 #testing additions
 #c.execute("CREATE TABLE IF NOT EXISTS logins(user TEXT, password TEXT, id INTEGER)")
@@ -15,18 +15,18 @@ c  = db.cursor()
 #c.execute("SELECT * FROM logins")
 #print(c.fetchall())
 
-#def main(user, passw):
-c.execute("CREATE TABLE IF NOT EXISTS logins(user TEXT, password TEXT, id INTEGER)")
-c.execute("SELECT * FROM logins")
-idvalue = len(c.fetchall())
-c.execute("INSERT INTO logins VALUES(?, ?, ?)", ("joe", "joey", idvalue))
-c.execute("SELECT * FROM logins")
-print(c.fetchall())
+def main(user, passw):
+	c.execute("CREATE TABLE IF NOT EXISTS logins(user TEXT, password TEXT, id INTEGER)")
+	c.execute("SELECT * FROM logins")
+	idvalue = len(c.fetchall())
+	c.execute("INSERT INTO logins VALUES(?, ?, ?)", (user, passw, idvalue))
+	c.execute("SELECT * FROM logins")
+	pprint(c.fetchall())
 
 
 
-#if __name__ == "__main__":
-
+if __name__ == "__main__":
+	main(sys.argv[1], sys.argv[2])
 
 db.commit()
 db.close()
